2f8e99f8a0e0faa824a75ac5989ff9b1d2294feb
rutaku12rutaku12 <chookyuho@gmail.com>
2025-06-09 16:27:06 +0900

    실습2까지

---
+++ day09/실습2.js
@@ -0,0 +1,27 @@
+// 문제 1
+console.log("[주문 접수]")
+console.log("이름")
+let order1 = prompt("이름을 적어주세요.")
+console.log(order1)
+console.log("메뉴")
+let order2 = prompt("원하는 음료를 적어주세요.")
+console.log(order2)
+console.log("수량")
+let order3 = prompt("원하는 수량을 적어주세요.")
+console.log(order3)
+
+
+// 문제 2
+console.log("[운동 기록]")
+console.log("이름")
+let info1 = prompt("이름을 적어주세요.")
+console.log(info1)
+console.log("운동종류")
+let info2 = prompt("오늘 한 운동 종류를 적어주세요.")
+console.log(info2)
+console.log("운동시간 (분)")
+const info3 = prompt("운동 시간(분 단위)를 적어주세요. (숫자만 적어주세요.)")
+console.log(info3)
+let cal = info3 * 7
+console.log("소모한 칼로리")
+console.log(cal)
