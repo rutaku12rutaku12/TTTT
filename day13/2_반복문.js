c8e5c21ac30b1d42b42bd18e363bfa504be67653
rutaku12rutaku12 <chookyuho@gmail.com>
2025-06-16 02:30:38 +0900

    2.10

--- day13/2_반복문.js
+++ day13/2_반복문.js
@@ -1,6 +1,54 @@
+// 문제 1: 1부터 10까지 출력하기
+// for 반복문을 사용하여 1부터 10까지의 숫자를 콘솔에 순서대로 출력하는 프로그램을 작성하시오.
+
+// for(let i= 1 ; i<=10; i++){console.log((i))}
+
+// 문제 2: 배열의 모든 요소 출력하기
+// 다음과 같은 과일 배열이 있습니다. for 반복문을 사용하여 배열의 모든 요소를 하나씩 콘솔에 출력하시오.
+// let fruits = ['사과', '바나나', '포도', '딸기'];
+
+// let fruits = ['사과', '바나나', '포도', '딸기'];
+// for(let i=0; i<=3; i++){console.log(fruits[i])}
+
+// 문제 3: 배열 역순으로 출력하기
+// 주어진 배열을 for 반복문을 사용하여 역순으로 (마지막 요소부터 첫 요소 순서로) 출력하는 프로그램을 작성하시오.
+// let numbers = [1, 2, 3, 4, 5];
+// 출력 예시: 5 4 3 2 1
+// let numbers = [1, 2, 3, 4, 5];
+
+// for(let i=4;i>=0;i--){console.log(numbers[i]);}
+
+// 문제 4: 1부터 50까지의 합계 구하기
+// for 반복문을 사용하여 1부터 50까지 모든 정수의 합을 구하여 콘솔에 출력하시오.
+// let sum= 0;
+
+// for(let i = 1; i<=50; i++){
+//     sum=sum+i;
+
+// }{console.log(sum)}
+
+
+
+// 문제 5: 짝수만 출력하기
+// for 반복문과 if 조건문을 사용하여, 1부터 20까지의 숫자 중 짝수만 콘솔에 출력하시오.
+
+// for (let i = 1 ; i<=20;i++){
+//     if(i%2===0){console.log(i)}
+// }
+
+
 // 문제 6: 배열 요소의 합계와 평균 구하기
-// 다음 학생들의 점수가 담긴 배열이 있습니다. for 반복문을 사용하여 모든 점수의 합계와 평균을 계산하여 콘솔에 출력하시오.
+// 다음 학생들의 점수가 담긴 배열이 있습니다. for 반복문을 사용하여 모든 점수의 합계와 평균을 
+// 계산하여 콘솔에 출력하시오.
 // let scores = [85, 92, 78, 65, 95];
+
+// let scores = [85, 92, 78, 65, 95];
+// let sum = 0;
+// for( let i = 0; i<=4 ; i++){
+//     sum=sum+scores[i];
+    
+// }console.log(`합계=${sum},평균=${(sum/5)}`)
+
 
 // * 배열내 모든 점수 조회 : 0부터 마지막인덱스까지 조회
 // for( let index = 0 ; index <= 마지막인덱스 ; index++ ){}  
@@ -16,9 +64,23 @@
 
 
 // 문제 7: 특정 조건에서 반복문 탈출하기 (break)
-// 점수 배열에서 100점 만점자가 처음 나타나면, 그 학생의 점수를 출력하고 반복문을 즉시 종료하는 프로그램을 작성하시오.
+// 점수 배열에서 100점 만점자가 처음 나타나면, 그 학생의 점수를 출력하고 
+// 반복문을 즉시 종료하는 프로그램을 작성하시오.
 // let scores = [77, 82, 100, 54, 96];
 // 출력 예시: 100점 만점자를 찾았습니다!
+
+// let scores = [77, 82, 100, 54, 96];
+
+// for(let i= 0; i<=scores.length-1;i++){
+//     if(scores[i]=='100'){
+//         console.log(`${scores[i]}점 만점자를 찾았습니다!`);
+//         break;
+//     }
+// }
+
+
+
+
 
 
 // let scores = [77, 82, 100, 54, 96];
@@ -36,6 +98,23 @@
 // 점수 배열에서 60점 미만(과락)인 점수는 건너뛰고, 60점 이상인 점수만 콘솔에 출력하는 프로그램을 작성하시오.
 // let scores = [90, 45, 72, 88, 59, 100];
 
+
+// let scores = [90, 45, 72, 88, 59, 100];
+// let list =[];
+// for(let i= 0; i<=scores.length-1;i++){
+//     if(scores[i]>=60){list.push(scores[i])}
+// }console.log(list)
+
+// let scores = [90, 45, 72, 88, 59, 100];
+// for(let i= 0; i<=scores.length-1;i++){
+//     let value=scores[i] 
+//     if(value<60){continue;}
+//     console.log(value)
+// }
+
+
+
+
 // let scores = [90, 45, 72, 88, 59, 100];
 // for ( let index = 0 ; index <= scores.length-1; index++){
 //     let value = scores[index];
@@ -45,8 +124,26 @@
 
 
 // 문제 9: 배열에서 특정 값의 개수 세기
-// 다음 배열에서 'A'형 혈액형을 가진 사람이 몇 명인지 for 반복문을 통해 세고, 그 수를 콘솔에 출력하시오.
+// 다음 배열에서 'A'형 혈액형을 가진 사람이 몇 명인지 for 반복문을 통해 세고, 
+// 그 수를 콘솔에 출력하시오.
 // let bloodTypes = ['A', 'B', 'O', 'AB', 'A', 'B', 'A'];
+
+
+
+// let bloodTypes = ['A', 'B', 'O', 'AB', 'A', 'B', 'A'];
+// let count=0;
+// for(let i= 0; i<=bloodTypes.length-1;i++){
+//         if(bloodTypes[i]!='A') continue;
+//         count+=1;
+        
+// }console.log(count)
+
+
+
+
+
+
+
 // let bloodTypes = ['A', 'B', 'O', 'AB', 'A', 'B', 'A'];
 // let countA = 0; // A형 개수를 저장하는 변수
 // for ( let index = 0 ; index <= bloodTypes.length-1; index++){
@@ -56,6 +153,14 @@
 //     }
 // } // for end
 // console.log(`A형 개수 : ${countA}`);
+
+
+
+
+
+
+
+
 
 // 문제 10: 학생 점수 시각화하기 
 // 주어진 학생 이름과 점수 배열을 이용하여, 각 학생의 점수를 동그라미(●, ○)로 시각화하여 HTML에 출력하는 프로그램을 작성하시오.
@@ -74,6 +179,14 @@
 // 강호동 ●●●●●●●●○○
 // 신동엽 ●●●●●●●○○○
 
+let nameArray = ['유재석', '강호동', '신동엽'];
+let scores = [92, 86, 72];
+
+for ( let index = 0 ; index <= nameArray.length-1; index++){
+    if(nameArray[i])
+    
+}
+......
 // let nameArray = ['유재석', '강호동', '신동엽'];
 // let scores = [92, 86, 72];
 
